45d820d070987c3103583cc5ea4340fbde27b75f
sanskar <sanskarjainskj@gmail.com>
2025-12-24 19:29:53 +0530

    Add initial GeoJSON files for Bengaluru and Bengaluru Main
    - Created 'bengaluru.geojson' with extensive geographical data.
    - Created 'bengaluru_main.geojson' containing primary geographical features.

--- vectura-core/src/main/java/com/vectura/core/domain/GeoCoordinate.java
+++ vectura-core/src/main/java/com/vectura/core/domain/GeoCoordinate.java
@@ -11,4 +11,25 @@ package com.vectura.core.domain;
  * </p>
  */
 public record GeoCoordinate(double latitude, double longitude) {
+    
+    private static final int EARTH_RADIUS_KM = 6371;
+
+    /**
+     * Calculates the Great Circle (Haversine) distance to another coordinate.
+     * @return Distance in Kilometers.
+     */
+    public double distanceTo(GeoCoordinate other) {
+        double dLat = Math.toRadians(other.latitude - this.latitude);
+        double dLon = Math.toRadians(other.longitude - this.longitude);
+
+        double lat1 = Math.toRadians(this.latitude);
+        double lat2 = Math.toRadians(other.latitude);
+
+        double a = Math.pow(Math.sin(dLat / 2), 2) +
+                   Math.pow(Math.sin(dLon / 2), 2) *
+                   Math.cos(lat1) * Math.cos(lat2);
+        
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return EARTH_RADIUS_KM * c;
+    }
 }
